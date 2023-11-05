86aea62f187b901e1341520352e4d0eb91d72be3
NogueiraDan <silvanogueira3460@gmail.com>
2023-11-05 14:56:35 -0300

    feat: responsividade

--- src/page/CustomersList/index.tsx
+++ src/page/CustomersList/index.tsx
@@ -6,16 +6,19 @@ import style from "./customerslist.module.css";
 export function CustomersList() {
   const [profissionalCustomers, setProfissionalCustomers] = useState<any>([]);
   useEffect(() => {
-    let user = JSON.parse(localStorage.getItem("user:customer") || '');
+    let user = JSON.parse(localStorage.getItem("user:customer") || "");
     let userToken = localStorage.getItem("token:customer");
 
     const headers = {
       Authorization: `Bearer ${userToken}`,
     };
     axios
-      .get(`https://customer-management-api-bdjh.onrender.com/profissionais/${user.id}/clientes`, {
-        headers,
-      })
+      .get(
+        `https://customer-management-api-bdjh.onrender.com/profissionais/${user.id}/clientes`,
+        {
+          headers,
+        }
+      )
       .then((res) => {
         console.log(res.data);
         setProfissionalCustomers(res.data);
@@ -24,16 +27,23 @@ export function CustomersList() {
   }, []);
   return (
     <>
+      <Header />
       <div className={style.container}>
-        <Header />
         <h1 className={style.title}>Sua carteira de Clientes</h1>
         <div>
           {profissionalCustomers.map((customer: any) => (
             <div className={style.customerWrapper}>
               <h3>Nome: {customer.nome}</h3>
-              <p><strong>Idade:</strong>{customer.idade}</p>
-              <p><strong>Endereço:</strong> {customer.endereco}</p>
-              <p><strong>Telefone para contato:</strong> {customer.telefone}</p>
+              <p>
+                <strong>Idade:</strong>
+                {customer.idade}
+              </p>
+              <p>
+                <strong>Endereço:</strong> {customer.endereco}
+              </p>
+              <p>
+                <strong>Telefone para contato:</strong> {customer.telefone}
+              </p>
             </div>
           ))}
         </div>
